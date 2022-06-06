9a4e0397807aade2c112f05eb5e0051e8dbc28fa
mitchelle <mitchellenjoki7@gmail.com>
2022-06-07 00:06:16 +0300

    Done.

--- index.js
+++ index.js
@@ -1,11 +1,33 @@
-function scuberGreetingForFeet(){
+ function scuberGreetingForFeet(feet) {
+if (feet <=400) {
+  return "This one is on me!" ;
+} else if (1999< feet && feet < 2500) {
+  return "I will gladly take your thirty bucks.";
+}
+else {
+  return "No can do.";
+}
+ }
+
+
+   
+  // Write your code here!
+
+function ternaryCheckCity(city) { 
+  return city==="NYC."? "Ok, sounds good." : "No go.";
+  
+  
   // Write your code here!
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
-
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tip){ 
+  switch (tip) {
+    case "generous"
+       :return  "Thank you so much."
+       case "not as generous"
+      : return "Thank you."
+       default :
+       return "Bye."
+  }
   // Write your code here!
 }
