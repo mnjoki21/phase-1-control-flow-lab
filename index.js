8b6d5112194cd9a974bdbf80ad40f14974e627c1
mitchelle <mitchellenjoki7@gmail.com>
2022-06-07 00:10:14 +0300

    Done.

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ else {
   // Write your code here!
 
 function ternaryCheckCity(city) { 
-  return city==="NYC."? "Ok, sounds good." : "No go.";
+  return city==="NYC"? "Ok, sounds good." : "No go.";
   
   
   // Write your code here!
